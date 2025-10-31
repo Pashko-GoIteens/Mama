cb7516a0b2d8f59594af29bf06018e6ff0948b8a
Pashko-GoIteens <paveltrofimenko08@gmail.com>
2025-10-31 21:04:15 +0300

    vdf

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  document.getElementById('intro').style.display = 'none';
+  const final = document.getElementById('final');
+  final.style.display = 'flex';
+}, 3000); // через 3 секунды показываем новые надписи
